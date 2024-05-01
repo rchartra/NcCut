b03d950e95436b48326d87dc3945239f539b9021
rchartra <rchartra@uw.edu>
2024-04-30 23:28:22 -0700

    No kivy log

--- cutview.py
+++ cutview.py
@@ -6,7 +6,6 @@ Builds app and file settings menu.
 import os
 import logging
 logging.getLogger('matplotlib.font_manager').disabled = True
-#logging.getLogger('kivy').setLevel(logging.INFO)
 os.environ["KIVY_NO_CONSOLELOG"] = "1"
 
 import kivy
