7606487d19dc7a486a2f9c2edbf6372cc7230758
rchartra <rchartra@uw.edu>
2024-07-12 09:49:52 -0700

    Change dimension default order

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -26,7 +26,7 @@ from cutview.cutview import CutView
 
 os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 SUPPORT_FILE_PATH = "support/"
-TEST_NC_PATH = pooch.retrieve(url="doi:10.5281/zenodo.12734428/test_nc.nc", known_hash=None)
+TEST_NC_PATH = pooch.retrieve(url="doi:10.5281/zenodo.12734574/test_nc.nc", known_hash=None)
 
 
 class AppInfo:
